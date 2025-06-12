7388f175b4fdcd01727a9d59f0f57fd1d9747825
Kshitiz Shrestha <yukishrestha50@gmail.com>
2025-06-12 14:17:58 +0545

    asg with launch template

--- TF-EC2/main.tf
+++ TF-EC2/main.tf
@@ -2,13 +2,46 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "kshitiz-intern" {
-  ami           = var.ami_id
+resource "aws_launch_template" "kshitiz_lt" {
+  name = "kshitiz-template"
+  image_id = var.ami_id
   instance_type = var.instance_type
-  key_name      = var.key_name
+  key_name = var.key_name
   vpc_security_group_ids = [var.security_group_ids]
 
-  tags = {
-    Name = "Kshitiz-EC2-instance"
+  user_data = <<EOF
+#!/bin/bash
+yum update -y
+yum install httpd -y
+service httpd start
+chkconfig httpd on
+cd /var/www/html
+echo "<html><h1>Welcome To My Webpage</h1></html>" > index.html
+EOF
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "Kshitiz-ASG-Instance"
+    }
+  }
+}
+
+resource "aws_autoscaling_group" "kshitiz_asg" {
+  availability_zones = ["us-east-1a"]
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.kshitiz_lt.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "Kshitiz-ASG-Instance"
+    propagate_at_launch = true
   }
 }
